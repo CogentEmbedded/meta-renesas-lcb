cb36f308fabfd591ae2192f2b07b360440515ae1
Andrey Dolnikov <andrey.dolnikov@cogentembedded.com>
2014-12-24 18:52:03 +0300

    Update copy_gfx_software... scripts to v115 version.

--- copy_gfx_software_porter.sh
+++ copy_gfx_software_porter.sh
@@ -10,5 +10,5 @@ then
   tar -C $1 -zxf $1/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-*.tar.gz
 fi
 
-cp $1/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers/sgx_gles2_m2_v114_eva/EVARTM0RC7791GLDE0001SL34C/EVA_SGX_KM_M2.tar.bz2 recipes-kernel/gles-module/gles-kernel-module/
-cp $1/R-Car_Series_Evaluation_Software_Package_for_Linux/sgx_gles2_m2_v114_eva/EVARTM0RC7791GLRE0001SL34C/release/EVA_r8a7791_linux_sgx_binaries_gles2.tar.bz2 recipes-graphics/gles-module/gles-user-module/
+cp $1/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers/sgx_gles2_m2_v115_eva/EVARTM0RC7791GLDE0001SL34C/EVA_SGX_KM_M2.tar.bz2 recipes-kernel/gles-module/gles-kernel-module/
+cp $1/R-Car_Series_Evaluation_Software_Package_for_Linux/sgx_gles2_m2_v115_eva/EVARTM0RC7791GLRE0001SL34C/release/EVA_r8a7791_linux_sgx_binaries_gles2.tar.bz2 recipes-graphics/gles-module/gles-user-module/
--- copy_gfx_software_silk.sh
+++ copy_gfx_software_silk.sh
@@ -10,5 +10,5 @@ then
   tar -C $1 -zxf $1/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-*.tar.gz
 fi
 
-cp $1/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers/sgx_gles2_e2_v114_eva/EVARTM0RC7794GLDE0001SL33C/EVA_SGX_KM_E2.tar.bz2 recipes-kernel/gles-module/gles-kernel-module/
-cp $1/R-Car_Series_Evaluation_Software_Package_for_Linux/sgx_gles2_e2_v114_eva/EVARTM0RC7794GLRE0001SL33C/release/EVA_r8a7794_linux_sgx_binaries_gles2.tar.bz2 recipes-graphics/gles-module/gles-user-module/
+cp $1/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers/sgx_gles2_e2_v115_eva/EVARTM0RC7794GLDE0001SL33C/EVA_SGX_KM_E2.tar.bz2 recipes-kernel/gles-module/gles-kernel-module/
+cp $1/R-Car_Series_Evaluation_Software_Package_for_Linux/sgx_gles2_e2_v115_eva/EVARTM0RC7794GLRE0001SL33C/release/EVA_r8a7794_linux_sgx_binaries_gles2.tar.bz2 recipes-graphics/gles-module/gles-user-module/
